8794790ded75e3428b13264edeb12d03b486628f
glenlcathey <glenlcathey@gmail.com>
2019-07-12 20:58:04 -0500

    start parameterization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  post '/', to: 'pages#generate_data'
+  delete '/', to: 'pages#stop_generating_data'
+  
+  #resources :pages do
+  #  collection do
+  #    get :generate_data
+  #  end
+  #end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

a5339f3c2da8cb3a142f99dcb84eaa88ffbf2700
glenlcathey <glenlcathey@gmail.com>
2019-07-28 02:50:11 -0500

    generate devise controllers, modify homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
-  post '/', to: 'pages#generate_data'
-  patch '/', to: 'pages#stop_generating_data'
-  delete '/', to: 'pages#delete_data'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    authenticated :user do
+      root 'pages#home', as: :authenticated_root
+    end
   
-  #resources :pages do
-  #  collection do
-  #    get :generate_data
-  #  end
-  #end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  post 'pages#home', to: 'pages#generate_data'
+  patch 'pages#home', to: 'pages#stop_generating_data'
+  delete 'pages#home', to: 'pages#delete_data'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
